1ceca53187d4d5a51602a82e15b8ddba8d67894b
MassiveZappy <massivezappy@gmail.com>
2024-07-23 00:02:17 -0400

    feat: ids and neighbors

--- temp.py
+++ temp.py
@@ -11,4 +11,20 @@ for d in data:
 	points.add(point(d["start"]))
 	points.add(point(d["end"]))
 
-print(len(points))
+idToCoord = {}
+neighbors = {}
+
+for d in data:
+	idToCoord[d["id"]] = [d["start"], d["end"]]
+	neighbors[d["id"]] = []
+
+for d in data:
+	print("\r", d["id"],"/",data[-1]["id"], end="")
+	for d2 in data:
+		if d["id"] != d2["id"]:
+			if d["end"] == d2["start"]:
+				neighbors[d["id"]].append(d2["id"])
+			if d["start"] == d2["end"]:
+				neighbors[d2["id"]].append(d["id"])
+
+print(neighbors)
