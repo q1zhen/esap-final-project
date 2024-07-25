cd77fe354fabd356fd4549253c19253ccb52f8f4
MassiveZappy <massivezappy@gmail.com>
2024-07-25 10:02:21 -0400

    more junk333

--- kmeans.py
+++ kmeans.py
@@ -4,7 +4,7 @@ from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 import json
 
-with open('nigelsMadness/pythonProject/output/england/uncleaned_england.json', 'r') as file:
+with open('nigelsMadness/pythonProject/output/kanto-latest-free.shp/uncleaned_nodes_kanto-latest-free.shp.json', 'r') as file:
 	raw = json.load(file)
 
 # coordinates = np.array(list(raw["nodes"].values()))
@@ -12,32 +12,32 @@ coordinates = np.array(raw)
 
 # k_max = 30
 # k_range = range(2, k_max)
-
+#
 # silhouette_scores = []
-
+#
 # for k in k_range:
-#     kmeans = KMeans(n_clusters=k, random_state=0).fit(coordinates)
-#     labels = kmeans.labels_
-#     score = silhouette_score(coordinates, labels)
-#     silhouette_scores.append(score)
-#     p = int((k / k_max) * 100)
-#     print("[", "#" * p, " " * (100 - p), "] ", p, "%", sep="", end="\r", flush=True)
+# 	kmeans = KMeans(n_clusters=k, random_state=0).fit(coordinates)
+# 	labels = kmeans.labels_
+# 	score = silhouette_score(coordinates, labels)
+# 	silhouette_scores.append(score)
+# 	p = int((k / k_max) * 100)
+# 	print("[", "#" * p, " " * (100 - p), "] ", p, "%", sep="", end="\r", flush=True)
 # print("Finished.", flush=True)
-
+#
 # optimal_k = k_range[np.argmax(silhouette_scores)]
-
+#
 # print(f"The optimal number of clusters (k) is: {optimal_k}")
-
+#
 # plt.plot(k_range, silhouette_scores, marker='o')
 # plt.title('Silhouette Scores for Different k Values')
 # plt.xlabel('Number of Clusters (k)')
 # plt.ylabel('Silhouette Score')
 # plt.show()
-
+#
 # kmeans = KMeans(n_clusters=optimal_k, random_state=0).fit(coordinates)
 # labels = kmeans.labels_
 # centers = kmeans.cluster_centers_
-
+#
 # plt.scatter(coordinates[:, 0], coordinates[:, 1], c=labels, cmap='viridis')
 # plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='x')
 # plt.title(f'K-Means Clustering with k={optimal_k}')
@@ -45,7 +45,8 @@ coordinates = np.array(raw)
 # plt.ylabel('Y Coordinate')
 # plt.show()
 
-for k in [1000]:
+for k in [2, 9, 16, 20, 27]:
+	print(k)
 	kmeans = KMeans(n_clusters=k, random_state=0).fit(coordinates)
 
 	labels = kmeans.labels_
